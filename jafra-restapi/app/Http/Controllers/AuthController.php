835fa7fd463ecc67ead74f22854b5f48eabe0a18
RansomBroker <yadisoke@gmail.com>
2022-05-21 19:12:33 +0700

    complete api REST

--- jafra-restapi/app/Http/Controllers/AuthController.php
+++ jafra-restapi/app/Http/Controllers/AuthController.php
@@ -136,6 +136,6 @@ class AuthController extends Controller
      */
     public function getAuthUser(Request $request )
     {
-        return response()->json($request->cookie('auth'));
+        return response()->json(auth()->user());
     }
 }
